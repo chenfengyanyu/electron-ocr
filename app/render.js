fc908d046081b8a8e1bcd460f35e1eb401722052
chenfengyanyu <jartto@qq.com>
2018-01-11 23:23:25 +0800

    jartto:add upload icon

--- app/render.js
+++ app/render.js
@@ -18,17 +18,29 @@ import {
 } from 'material-ui/styles/colors';
 import getMuiTheme from 'material-ui/styles/getMuiTheme';
 import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider';
+import ActionBackup from 'material-ui/svg-icons/action/backup';
 
 import Block from './components/Block';
 import AppBar from './components/AppBar';
 import SideMenu from './components/Menu';
 
-let muiTheme = getMuiTheme({
+const muiTheme = getMuiTheme({
   fontFamily: 'Roboto, sans-serif',
   palette: {
     // primary1Color: white,
   }
 });
+
+const iconStyles = {
+  width: '100px',
+  height: '100px',
+  fill: '#ececec',
+  position: 'relative',
+  top: '50%',
+  left: '50%',
+  margin: '-50px 0 0 -50px',
+  cursor: 'pointer'
+};
 
 class MainWindow extends React.Component {
 
@@ -46,7 +58,9 @@ class MainWindow extends React.Component {
       <MuiThemeProvider muiTheme={muiTheme}>
         <div>
           <SideMenu />
-          <div className="box"></div>
+          <div className="box">
+            <ActionBackup style={iconStyles}/>
+          </div>
         </div>
       </MuiThemeProvider>
     );
