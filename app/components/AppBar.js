66cc6debe984fe3c928d8dc1266605bd7a0d5d53
chenfengyanyu <jartto@qq.com>
2018-01-11 19:01:02 +0800

    jartto:add metrial icons

--- app/components/AppBar.js
+++ app/components/AppBar.js
@@ -2,7 +2,7 @@ import React from 'react';
 import AppBar from 'material-ui/AppBar';
 import Avatar from 'material-ui/Avatar';
 import IconButton from 'material-ui/IconButton';
-import NavigationClose from 'material-ui/svg-icons/navigation/close';
+import NavigationMenu from 'material-ui/svg-icons/navigation/menu';
 
 /**
  * A simple example of `AppBar` with an icon on the right.
@@ -10,7 +10,7 @@ import NavigationClose from 'material-ui/svg-icons/navigation/close';
  */
 const AppBarIcon = () => (
   <AppBar
-    iconElementLeft={<IconButton><NavigationClose /></IconButton>}
+    iconElementLeft={<IconButton><NavigationMenu /></IconButton>}
     iconElementRight= {<Avatar src="image/icon.png" />}
   />
 );
--- app/components/Menu.js
+++ app/components/Menu.js
@@ -2,6 +2,15 @@ import React from 'react';
 import Drawer from 'material-ui/Drawer';
 import MenuItem from 'material-ui/MenuItem';
 import RaisedButton from 'material-ui/RaisedButton';
+import {List, ListItem} from 'material-ui/List';
+import ActionInfo from 'material-ui/svg-icons/action/info';
+import Avatar from 'material-ui/Avatar';
+import FileFolder from 'material-ui/svg-icons/file/folder';
+import ActionBackup from 'material-ui/svg-icons/action/backup';
+import ActionVoice from 'material-ui/svg-icons/action/settings-voice';
+import ActionFavorite from 'material-ui/svg-icons/action/favorite';
+import {blue500, yellow600, pink300, green400} from 'material-ui/styles/colors';
+import EditorInsertChart from 'material-ui/svg-icons/editor/insert-chart';
 
 export default class DrawerUndockedExample extends React.Component {
 
@@ -27,12 +36,32 @@ export default class DrawerUndockedExample extends React.Component {
         />
         <Drawer
           docked={false}
-          width={250}
+          width={230}
           open={this.state.open}
           onRequestChange={(open) => this.setState({open})}
         >
-          <MenuItem onClick={this.handleClose.bind(this)}>Menu Item</MenuItem>
-          <MenuItem onClick={this.handleClose.bind(this)}>Menu Item 2</MenuItem>
+          <List>
+            <ListItem
+              leftAvatar={<Avatar icon={<ActionBackup />} backgroundColor={blue500} />}
+              rightIcon={<ActionInfo />}
+              primaryText="上传图片"
+            />
+            <ListItem
+              leftAvatar={<Avatar icon={<EditorInsertChart />} backgroundColor={yellow600} />}
+              rightIcon={<ActionInfo />}
+              primaryText="文字识别"
+            />
+            <ListItem
+              leftAvatar={<Avatar icon={<ActionVoice />} backgroundColor={pink300} />}
+              rightIcon={<ActionInfo />}
+              primaryText="语音朗读"
+            />
+            <ListItem
+              leftAvatar={<Avatar icon={<ActionFavorite />} backgroundColor={green400} />}
+              rightIcon={<ActionInfo />}
+              primaryText="联系我们"
+            />
+          </List>
         </Drawer>
       </div>
     );
